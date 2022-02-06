bbd9280c5b8cfaa68d43f39c0f056424cfae60d6
Jared Lewis <jlewis8493@gmail.com>
2022-02-06 15:50:30 -0500

    Taco Parser Build Successful

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -1,5 +1,6 @@
 using System;
 using Xunit;
+using System.Collections.Generic;
 
 namespace LoggingKata.Test
 {
@@ -8,15 +9,11 @@ namespace LoggingKata.Test
         [Fact]
         public void ShouldDoSomething()
         {
-            // TODO: Complete Something, if anything
 
-            //Arrange
             var tacoParser = new TacoParser();
 
-            //Act
             var actual = tacoParser.Parse("34.073638, -84.677017, Taco Bell Acwort...");
 
-            //Assert
             Assert.NotNull(actual);
 
         }
@@ -25,22 +22,25 @@ namespace LoggingKata.Test
         [InlineData("34.073638, -84.677017, Taco Bell Acwort...", -84.677017)]
         public void ShouldParseLongitude(string line, double expected)
         {
-            // TODO: Complete - "line" represents input data we will Parse to
-            //       extract the Longitude.  Your .csv file will have many of these lines,
-            //       each representing a TacoBell location
 
-            //Arrange - write the code we need in order to call the method we're testing
             var tacoParserInstance = new TacoParser();
 
-            //Act
-            var actual = tacoParserInstance.Parse(line).Location.Longitude;
+            var actual = tacoParserInstance.Parse(line);
 
-            //Assert
-            Assert.Equal(expected, actual);
+            Assert.Equal(expected, actual.Location.Longitude);
         }
 
 
-        //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLat(string line, double expected)
+        {
 
+            var tacoParserInstance = new TacoParser();
+
+            var actual = tacoParserInstance.Parse(line);
+
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
     }
 }
